cdf854f98a4745036a01d7732ccae65f29ee154a
Thilina Madumal <thilina.madumal@trycake.com>
2016-12-21 18:44:06 +0530

    testing ssh

--- src/components/Rectangle.jsx
+++ src/components/Rectangle.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 export const Rectangle = () => {
   return <div>
-    <h1>Hot loading Replacement</h1>
-    <h2>works fine</h2>
-    <h3>change and see..</h3>
+    <h1>Rectange</h1>
   </div>;
 };
