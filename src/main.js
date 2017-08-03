56a08045207251ee385448034365c78130aa34f2
thilina <madumalt@gmail.com>
2017-08-03 11:25:35 +0530

    adding a compatible webpack-dev-server with webpack

--- src/main.js
+++ src/main.js
@@ -6,13 +6,16 @@ import {
  SVG_HEIGHT,
 } from './Constants';
 
-ReactDom.render(<svg
-  width={SVG_WIDTH}
-  height={SVG_HEIGHT}
-  viewBox={[0, 0, SVG_WIDTH, SVG_HEIGHT]}>
-  <BlockShape translateX={10} translateY={10} color='orange' shapeType={0} />
-  <BlockShape translateX={60} translateY={60} color='slategrey' shapeType={1} />
-  <BlockShape translateX={120} translateY={65} color='slategrey' shapeType={2} />
-  <BlockShape translateX={60} translateY={120} color='slategrey' shapeType={3} />
-</svg>, document.getElementById('app'));
+ReactDom.render(
+    <svg
+      width={SVG_WIDTH}
+      height={SVG_HEIGHT}
+      viewBox={[0, 0, SVG_WIDTH, SVG_HEIGHT]}>
+      <BlockShape translateX={10} translateY={10} color='orange' shapeType={0} />
+      <BlockShape translateX={60} translateY={60} color='slategrey' shapeType={1} />
+      <BlockShape translateX={120} translateY={65} color='slategrey' shapeType={2} />
+      <BlockShape translateX={60} translateY={120} color='slategrey' shapeType={3} />
+    </svg>,
+    document.getElementById('app')
+);
 module.hot.accept();
